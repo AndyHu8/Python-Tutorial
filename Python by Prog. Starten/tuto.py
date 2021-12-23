2caeecbc0d2abbee9ceb76dbf122d9a89b8f9517
胡龙 Hulong <47930486+AndyHu8@users.noreply.github.com>
2021-12-23 23:13:22 +0100

    Add files via upload

---
+++ Python by Prog. Starten/tuto.py
@@ -0,0 +1,13 @@
+#Python Tuto #1 Dein erstes Programm
+#Python und IDE installieren
+#Projekt erstellen (Python-Datei erstellen und mit IDE öffnen)
+
+#Hallo Welt
+print("Hallo Welt")
+
+#Python Tuto #2 Zahlen in Py
+print(10, -7, 7 + 4)
+print((3 + 2) * (2 + 3))
+print(3.4)
+
+#Python Tuto #3 Strings in Py
