0035015081250d132b44b561b93159e638ee1d8f
AndyHu8 <huandy01@gmail.com>
2021-12-27 13:45:44 +0100

    #11

--- Python by Prog. Starten/tuto.py
+++ Python by Prog. Starten/tuto.py
@@ -11,3 +11,46 @@ print((3 + 2) * (2 + 3))
 print(3.4)
 
 #Python Tuto #3 Strings in Py
+print("Ein String")
+print("Das" + "Dies")
+print('''Mehr
+        zeilig''')
+
+#Python Tuto #4 Variablen
+age = 20
+print(age)
+
+#Python Tuto #5 Datyentypen
+print("String" + "Verkettung")
+print(10 + 3)
+#print(10 + "String") #Geht nicht
+
+#Python Tuto #6 input()-Funktion
+print("Start")
+inputName = input()
+print("Ich heiße " + inputName)
+print("Ende")
+
+#Python Tuto #7 Type-Casting-Funktionen
+value1 = input("Erste Zahl: ")
+value2 = input("Zweite Zahl: ")
+print(int(value1) + int(value2))
+
+#Python Tuto #8 Zuweisungsoperator
+example = 22
+example += 22
+
+#Python Tuto #9 Vergleichsoperator und bool
+print(1 < 5)
+print(1 <= 5)
+print(1 != 5)
+wahr = True
+falsch = False
+
+#Python Tuto #10 If-Anweisung
+age = int(input("Dein Alter: "))
+
+if age < 18:
+    print("Du bist minderjährig")
+
+#Python Tuto #11 Elif und Else Zweige
